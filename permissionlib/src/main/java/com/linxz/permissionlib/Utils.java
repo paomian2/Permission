0da89e65f9e680ddf161658ef353c6f63accc774
linxiaozhang <xiaozhang.lin@jinbenteng.com>
2018-09-21 08:46:36 +0800

    FirstCommit

---
+++ permissionlib/src/main/java/com/linxz/permissionlib/Utils.java
@@ -0,0 +1,17 @@
+package com.linxz.permissionlib;
+
+/**
+ * <p>
+ * Function： TODO
+ * <p>
+ * ver     date      		author
+ * ──────────────────────────────────
+ * V1.0   2018年09月21日8:41  lin_xiao_zhang@163.com
+ * <p>
+ * Copyright (c) 2018,  All Rights Reserved.
+ *
+ * @author linxz
+ */
+public class Utils {
+    public final static int CONFIG=1001;
+}
